d108cb30990fcc3471d85782617c35d60ebc8d4c
Ahmet Can Suntay <suntayahmetcan@gmail.com>
2025-10-22 00:52:59 +0300

    Hesap Makinesi toplama çıkarma fonksiyonları eklendi

---
+++ BmgOdevHesapMakinesi/BmgOdevHesapMakinesi/BmgOdevHesapMakinesi.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    double sayi1, sayi2, sonuc;
+    char islem;
+
+    cout << "=== Basit Hesap Makinesi ===" << endl;
+    cout << "İslemler: + (Toplama), - (Cıkarma)" << endl;
+
+    cout << "Birinci sayiyi girin: ";
+    cin >> sayi1;
+
+    cout << "İslemi secin (+ veya -): ";
+    cin >> islem;
+
+    cout << "İkinci sayiyi girin: ";
+    cin >> sayi2;
+
+    if (islem == '+') {
+        sonuc = sayi1 + sayi2;
+        cout << "Sonuc: " << sayi1 << " + " << sayi2 << " = " << sonuc << endl;
+    }
+    else if (islem == '-') {
+        sonuc = sayi1 - sayi2;
+        cout << "Sonuc: " << sayi1 << " - " << sayi2 << " = " << sonuc << endl;
+    }
+    else {
+        cout << "Geçersiz islem girdiniz! Lütfen sadece + veya - kullanin." << endl;
+    }
+
+    return 0;
+}
